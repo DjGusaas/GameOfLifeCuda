4b9f644b11343b67e06e70cef3e412886ba347ba
DjGusaas <gusaa004@morris.umn.edu>
2015-12-08 19:37:34 -0600

    Added initial cuda file as well as cuda example and non-parallel version in C

---
+++ GameOfLife.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define for_x for (int x = 0; x < w; x++)
+#define for_y for (int y = 0; y < h; y++)
+#define for_xy for_x for_y
+__global__ void blah()
+{
+  int id = blockId
+}
+
+int main()
+{
+
+	return 0;
+}

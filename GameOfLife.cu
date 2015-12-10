d55cad954d7846272a2a4fea56e9e2584accef68
DjGusaas <gusaa004@morris.umn.edu>
2015-12-09 18:58:04 -0600

    Couple of changes

--- GameOfLife.cu
+++ GameOfLife.cu
@@ -2,16 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define for_x for (int x = 0; x < w; x++)
-#define for_y for (int y = 0; y < h; y++)
-#define for_xy for_x for_y
-__global__ void blah()
+__global__ void cell()
 {
-  int id = blockId
+	int id = blockIdx.x * blockDim.x + threadIdx.x;
+	
+	// TODO
+}
+
+void generate(int g)
+{
+	int univ[][];
+	
+	for () {
+		
+	}
+
+	while(g > 0)
+	{
+
+		g--;
+	}
 }
 
 int main()
 {
+	int g;
 
-	return 0;
+	printf("Enter desired number of generations:\n");
+	scanf("%d", &g);
+	generate(g);
 }

9f9642e5450c16b246484f2b4acf27892722ef4b
DjGusaas <gusaa004@morris.umn.edu>
2016-03-07 15:31:35 -0600

    messing with print function

--- src/non-parallel/non-parallel-game-of-life.c
+++ src/non-parallel/non-parallel-game-of-life.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_array(int arr[], int size)
+void print_univ(int arr[], int size)
 {
 	printf("\n");
 
-	for (int i = 0; i < size; i++)
-	{
-		printf("%d", arr[i]);
+	for (int i = 0; i < size; i++) {
+		if (i % 3 == 0 && i == size) {
+			printf("\n");
+			printf("%d", arr[i]);
+		} else {
+			printf("%d", arr[i]);
+		}
 	}
 
 	printf("\n");
@@ -21,34 +25,31 @@ void generate(int g, int h, int w)
 	int univ[size];
 	int new_univ[size];
 
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < size; i++) {
 		univ[i] = rand() % 2;
-	}
+	}	
 
 	printf("Initial universe:");
-	print_array(univ, size);
+	print_univ(univ, size);
 
-	while(g > 0)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			int alive = 0
+	while (g > 0){
+		for (int i = 0; i < size; i++){
+			int alive = 0;
 
-			unsigned x_pos = i % w;
-			unsigned y_pos = i - x_pos;
-			unsigned x_left = (x_pos + w - 1) % w;
-			unsigned x_right = (x_pos + 1) % w;
-			unsigned y_above = (y_pos + size - w) % size;
-			unsigned y_below = (y_pos + size) % size;
+			unsigned x = i % w;
+			unsigned y = i - x;
+			unsigned xl = (x + w - 1) % w;
+			unsigned xr = (x + 1) % w;
+			unsigned yu = (y + size - w) % size;
+			unsigned yd = (y + size) % size;
 
-			alive = ;	
+			//alive = ;	
 
 			new_univ[i] = (alive == 3 || (alive == 2 && univ[i]) ? 1 : 0);
 		}			
 
 		memcpy(univ, new_univ, size);
-		print_array(univ, size);
+		print_univ(univ, size);
 		g--;
 	}
 }
